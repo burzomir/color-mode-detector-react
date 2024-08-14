d47ace087453537d04f4e34e6d63a4db5c131ded
burzomir <burzomirdev@gmail.com>
2024-08-14 22:54:41 +0200

    Configured eslint to work with prettier

--- packages/react/eslint.config.mjs
+++ packages/react/eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   { ignores: ["**/dist/**"] },
@@ -11,4 +12,5 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat["jsx-runtime"],
+  eslintConfigPrettier,
 ];
